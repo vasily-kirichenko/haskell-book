8ab260514d65430ae29713e260daefb3571d7fdd
vaskir <vaskir@gmail.com>
2016-09-16 21:42:56 +0300

    add Functors.hs

---
+++ src/Functors.hs
@@ -0,0 +1,11 @@
+module Functors where
+
+a = (+1) <$> read "[1]" :: [Int]
+b = (fmap . fmap) (++ "lol") (Just ["Hi,", "Hello"])
+c = fmap (*2) (\x -> x - 2)
+d = ((return '1' ++) . show) `fmap` (\x -> [x, 1..3])
+
+e :: IO Integer
+e = let ioi = readIO "1" :: IO Integer
+        changed = fmap (read . ("123"++)) (fmap show ioi)
+    in fmap (*3) changed
