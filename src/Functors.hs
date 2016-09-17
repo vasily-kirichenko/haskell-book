b1cc9f3096bd062531a58494cd0641a4acc4d326
vaskir <vaskir@gmail.com>
2016-09-17 09:26:53 +0300

    more Functor instances

--- src/Functors.hs
+++ src/Functors.hs
@@ -47,6 +47,66 @@ instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
     b <- arbitrary
     return $ Two a b
 
+-- Three a b c
+
+data Three a b c = Three a b c deriving (Eq, Show)
+
+instance Functor (Three a b) where
+  fmap f (Three a b c) = Three a b (f c)
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c) =>
+  Arbitrary (Three a b c) where
+    arbitrary = do
+      a <- arbitrary
+      b <- arbitrary
+      c <- arbitrary
+      return $ Three a b c
+
+-- Three' a b b
+
+data Three' a b = Three' a b b deriving (Eq, Show)
+
+instance Functor (Three' a) where
+  fmap f (Three' a b b') = Three' a (f b) (f b')
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Three' a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    b' <- arbitrary
+    return $ Three' a b b'
+
+-- Four a b c d
+
+data Four a b c d = Four a b c d deriving (Eq, Show)
+
+instance Functor (Four a b c) where
+  fmap f (Four a b c d) = Four a b c (f d)
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c, Arbitrary d) =>
+  Arbitrary (Four a b c d) where
+    arbitrary = do
+      a <- arbitrary
+      b <- arbitrary
+      c <- arbitrary
+      d <- arbitrary
+      return $ Four a b c d
+
+-- Four' a b = Four' a a a b
+
+data Four' a b = Four' a a a b deriving (Eq, Show)
+
+instance Functor (Four' a) where
+  fmap f (Four' a a' a'' b) = Four' a a' a'' (f b)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Four' a b) where
+  arbitrary = do
+    a <- arbitrary
+    a' <- arbitrary
+    a'' <- arbitrary
+    b <- arbitrary
+    return $ Four' a a' a'' b
+
 main :: IO ()
 main = do
   quickCheck $ \x -> functorIdentity (x :: Identity Int)
@@ -57,3 +117,15 @@ main = do
 
   quickCheck $ \x -> functorIdentity (x :: Two Int Int)
   quickCheck $ \x -> functorCompose (+1) (*2) (x :: Two Int Int)
+
+  quickCheck $ \x -> functorIdentity (x :: Three Int Int String)
+  quickCheck $ \x -> functorCompose (+1) (*2) (x :: Three String String Int)
+
+  quickCheck $ \x -> functorIdentity (x :: Three' Int Int)
+  quickCheck $ \x -> functorCompose (+1) (*2) (x :: Three' String Int)
+
+  quickCheck $ \x -> functorIdentity (x :: Four Int Int Int Int)
+  quickCheck $ \x -> functorCompose (+1) (*2) (x :: Four String String String Int)
+
+  quickCheck $ \x -> functorIdentity (x :: Four' Int Int)
+  quickCheck $ \x -> functorCompose (+1) (*2) (x :: Four' String Int)
