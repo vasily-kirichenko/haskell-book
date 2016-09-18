44f230f40ce0ee639199edf99669b3f13daba867
vaskir <vaskir@gmail.com>
2016-09-18 18:59:38 +0300

    Sum Monad

--- src/Monads.hs
+++ src/Monads.hs
@@ -1,6 +1,23 @@
 module Monads where
 
-import           Control.Monad (join)
+import           Data.Monoid hiding (First, Second, Sum)
 
-bind :: Monad m => (a -> m b) -> m a -> m b
-bind f = join . fmap f
+data Sum a b
+  = First a
+  | Second b
+  deriving (Eq, Show)
+
+instance Functor (Sum a) where
+  fmap _ (First a) = First a
+  fmap f (Second b) = Second (f b)
+
+instance Applicative (Sum a) where
+  pure = Second
+  First a <*> _ = First a
+  _ <*> First a = First a
+  Second f <*> Second x = Second $ f x
+
+instance Monad (Sum a) where
+  return = pure
+  First a >>= _ = First a
+  Second b >>= f = f b
