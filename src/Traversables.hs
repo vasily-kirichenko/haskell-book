822c0896cbd4052e62c2b2ff8f715e22525824d4
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2016-09-22 15:44:15 +0300

    finish Traversable

--- src/Traversables.hs
+++ src/Traversables.hs
@@ -2,6 +2,7 @@
 
 module Traversables where
 
+import           Data.Monoid              ((<>))
 import           Test.QuickCheck
 import           Test.QuickCheck.Checkers
 import           Test.QuickCheck.Classes
@@ -161,6 +162,42 @@ instance Arbitrary a => Arbitrary (S Maybe a) where
 
 instance Eq a => EqProp (S Maybe a) where (=-=) = eq
 
+-- Tree
+
+data Tree a
+  = Empty
+  | Leaf a
+  | Node (Tree a) a (Tree a)
+  deriving (Eq, Show)
+
+instance Functor Tree where
+  fmap _ Empty = Empty
+  fmap f (Leaf a) = Leaf (f a)
+  fmap f (Node l a r) = Node (fmap f l) (f a) (fmap f r)
+
+instance Foldable Tree where
+  foldMap _ Empty = mempty
+  foldMap f (Leaf a) = f a
+  foldMap f (Node l a r) = foldMap f l <> f a <> foldMap f r
+
+instance Traversable Tree where
+  traverse _ Empty = pure Empty
+  traverse f (Leaf a) = Leaf <$> f a
+  traverse f (Node l a r) =
+    Node
+    <$> traverse f l
+    <*> f a
+    <*> traverse f r
+
+instance Arbitrary a => Arbitrary (Tree a) where
+  arbitrary = do
+    a <- arbitrary
+    l <- arbitrary
+    r <- arbitrary
+    elements [ Empty, Leaf a, Node l a r ]
+
+instance Eq a => EqProp (Tree a) where (=-=) = eq
+
 main :: IO ()
 main = do
     quickBatch $ traversable (undefined :: Identity (Int, Int, [Int]))
@@ -169,5 +206,5 @@ main = do
     quickBatch $ traversable (undefined :: List (Int, Int, [Int]))
     quickBatch $ traversable (undefined :: Three Int Int (Int, Int, [Int]))
     quickBatch $ traversable (undefined :: Three' Int (Int, Int, [Int]))
-    quickBatch $
-      traversable (undefined :: S Maybe (Int, Int, [Int]))
+    quickBatch $ traversable (undefined :: S Maybe (Int, Int, [Int]))
+    quickBatch $ traversable (undefined :: Tree (Int, Int, [Int]))
