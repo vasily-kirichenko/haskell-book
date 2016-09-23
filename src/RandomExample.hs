d48595be7f3573edb8dda6ca2896652dea9e4d50
vaskir <vaskir@gmail.com>
2016-09-23 22:02:32 +0300

    continue RandomExample

--- src/RandomExample.hs
+++ src/RandomExample.hs
@@ -1,5 +1,8 @@
 module RandomExample where
 
+import           Control.Applicative       (liftA3)
+import           Control.Monad             (replicateM)
+import           Control.Monad.Trans.State
 import           System.Random
 
 data Die
@@ -21,3 +24,22 @@ intToDie n =
     5 -> DieFive
     6 -> DieSix
     _ -> error $ "intToDie got non 1-6 integer: " ++ show n
+
+rollDieThreeTimes :: (Die, Die, Die)
+rollDieThreeTimes = do
+  let s = mkStdGen 0
+      (d1, s1) = randomR (1, 6) s
+      (d2, s2) = randomR (1, 6) s1
+      (d3, _) = randomR (1, 6) s2
+  (intToDie d1, intToDie d2, intToDie d3)
+
+rollDie :: State StdGen Die
+rollDie = state $ do
+  (n, s) <- randomR (1, 6)
+  return (intToDie n, s)
+
+rollDie' :: State StdGen Die
+rollDie' = intToDie <$> state (randomR (1, 6))
+
+rollDieThreeTimes' :: State StdGen (Die, Die, Die)
+rollDieThreeTimes' = liftA3 (,,) rollDie' rollDie' rollDie'
