ec354a3ff282cd590575cf8c6abbe1b98460e801
vaskir <vaskir@gmail.com>
2016-09-24 14:46:53 +0300

    die game

--- src/RandomExample.hs
+++ src/RandomExample.hs
@@ -43,3 +43,39 @@ rollDie' = intToDie <$> state (randomR (1, 6))
 
 rollDieThreeTimes' :: State StdGen (Die, Die, Die)
 rollDieThreeTimes' = liftA3 (,,) rollDie' rollDie' rollDie'
+
+infiniteDie :: State StdGen [Die]
+infiniteDie = repeat <$> rollDie
+
+nDie :: Int -> State StdGen [Die]
+nDie n = replicateM n rollDie
+
+rollsToGetTwenty :: StdGen -> Int
+rollsToGetTwenty = go 0 0
+  where
+    go :: Int -> Int -> StdGen -> Int
+    go sum count gen
+      | sum >= 20 = count
+      | otherwise =
+          let (die, nextGen) = randomR (1, 6) gen
+          in go (sum + die) (count + 1) nextGen
+
+rollsToGetN :: Int -> StdGen -> Int
+rollsToGetN n = go 0 0
+  where
+    go :: Int -> Int -> StdGen -> Int
+    go sum count gen
+      | sum >= n = count
+      | otherwise =
+          let (die, nextGen) = randomR (1, 6) gen
+          in go (sum + die) (count + 1) nextGen
+
+rollsCountLogged :: Int -> StdGen -> (Int, [Die])
+rollsCountLogged n = go 0 (0, [])
+  where
+    go :: Int -> (Int, [Die]) -> StdGen -> (Int, [Die])
+    go sum s@(count, dies) gen
+      | sum >= n = s
+      | otherwise =
+          let (die, nextGen) = randomR (1, 6) gen
+          in go (sum + die) (count + 1, intToDie die:dies) nextGen
