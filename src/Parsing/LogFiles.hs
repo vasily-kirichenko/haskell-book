3a429366e8d89d8befa7df17f799a59bea10b0f2
vaskir <vaskir@gmail.com>
2016-09-29 21:59:41 +0300

    continue log parsing

--- src/Parsing/LogFiles.hs
+++ src/Parsing/LogFiles.hs
@@ -9,10 +9,22 @@ import           Data.Time           hiding (parseTime)
 import           Text.RawString.QQ
 import           Text.Trifecta
 
-data Log = Log [LogDay] deriving (Eq, Show)
-data LogDay = LogDay Day [Activity] deriving (Eq, Show)
+data Log = Log [LogDay] deriving Eq
+data LogDay = LogDay Day [Activity] deriving Eq
 type Name = String
-data Activity = Activity TimeOfDay Name deriving (Eq, Show)
+data Activity = Activity TimeOfDay Name deriving Eq
+
+instance Show Activity where
+  show (Activity time name) = show time ++ " " ++ name
+
+instance Show LogDay where
+  show (LogDay day activities) =
+    "\n# " ++ show day ++ "\n" ++
+    foldr (\a r -> show a ++ "\n" ++ r) "" activities
+
+instance Show Log where
+  show (Log logDays) =
+    foldr (\x r -> show x ++ "\n" ++ r) "" logDays
 
 parseTime :: Parser TimeOfDay
 parseTime = do
@@ -40,9 +52,9 @@ parseDay = do
   char '#'
   whiteSpace
   year <- integer
-  _ <- char '-'
+  char '-'
   month <- fromInteger <$> integer
-  _ <- char '-'
+  char '-'
   day <- fromInteger <$> integer
   skipMany $ noneOf "\n"
   eol
@@ -54,8 +66,21 @@ parseLogDay = LogDay <$> parseDay <*> some parseActivity
 parseLog :: Parser Log
 parseLog = Log <$> many (whiteSpace *> parseLogDay)
 
-logDay :: String
-logDay = [r|
+logEx :: String
+logEx = [r|
+# 2025-02-05
+08:00 Breakfast
+09:00 Sanitizing moisture collector
+11:00 Exercising in high-grav gym
+12:00 Lunch
+13:00 Programming
+17:00 Commuting home in rover
+17:30 R&R
+19:00 Dinner
+21:00 Shower
+21:15 Read
+22:00 Sleep
+
 # 2025-02-07 -- dates not nececessarily sequential
 08:00 Breakfast -- should I try skippin bfast?
 09:00 Bumped head, passed out
