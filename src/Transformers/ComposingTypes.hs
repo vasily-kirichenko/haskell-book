f908bf29b8d062907a0a5d2500ea382bbf93e47c
vaskir <vaskir@gmail.com>
2016-10-01 09:40:51 +0300

    Applicative Compose done!

--- src/Transformers/ComposingTypes.hs
+++ src/Transformers/ComposingTypes.hs
@@ -12,14 +12,6 @@ instance (Functor f, Functor g) => Functor (Compose f g) where
   fmap f (Compose fga) = Compose $ (fmap . fmap) f fga
 
 instance (Applicative f, Applicative g) => Applicative (Compose f g) where
-  pure :: a -> Compose f g a
   pure a = Compose $ pure (pure a)
-
-  (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b
   Compose fgab <*> Compose fga =
-
-    let fgb = _ fgab fga
-
---    let fgb = fmap (\gab -> fmap (\ga -> gab <*> ga) fga) fgab
-
-    in Compose fgb
+    Compose $ (<*>) <$> fgab <*> fga
