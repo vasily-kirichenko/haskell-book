3a5a421741ee0f36da688d4998d86a20d0644cbd
vaskir <vaskir@gmail.com>
2016-10-01 11:46:44 +0300

    Foldable and Traversable for Compose

--- src/Transformers/ComposingTypes.hs
+++ src/Transformers/ComposingTypes.hs
@@ -15,3 +15,9 @@ instance (Applicative f, Applicative g) => Applicative (Compose f g) where
   pure a = Compose $ pure (pure a)
   Compose fgab <*> Compose fga =
     Compose $ (<*>) <$> fgab <*> fga
+
+instance (Foldable f, Foldable g) => Foldable (Compose f g) where
+  foldMap fm (Compose fga) = (foldMap . foldMap) fm fga
+
+instance (Traversable f, Traversable g) => Traversable (Compose f g) where
+  traverse afb (Compose fga) = Compose <$> (traverse . traverse) afb fga
