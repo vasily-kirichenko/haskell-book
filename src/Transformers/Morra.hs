2aeb430a1fe14047f16395264d29b279428ebf25
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2016-10-05 11:51:55 +0300

    Morra: computer examine human previous guesses to make better player itself

--- src/Transformers/Morra.hs
+++ src/Transformers/Morra.hs
@@ -3,6 +3,8 @@ module Morra where
 import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Class
 import           Control.Monad.Trans.State
+import qualified Data.List                 as L
+import           Data.Maybe                (fromMaybe)
 import           System.Random
 
 data Scores =
@@ -17,54 +19,84 @@ data GameState =
   , allPlayerGuesses :: [Int]
 } deriving Show
 
+getPreviousPlayerGuess :: [Int] -> Maybe Int
+getPreviousPlayerGuess guesses =
+  case reverse . fst . L.splitAt 2 $ guesses of
+    [x, y] ->
+      let guesses' = reverse . drop 1 $ guesses in
+      do i <- L.elemIndex x guesses'
+         case drop i guesses' of
+           _ : y' : z : _
+            | y == y' -> Just z
+            | otherwise -> Nothing
+           _ -> Nothing
+    _ -> Nothing
+
 getComputerGuess :: Int -> StateT GameState IO Int
 getComputerGuess maxNumber = do
-  liftIO $ randomRIO (0, maxNumber)
+  playerGuesses <- allPlayerGuesses <$> get
+  case getPreviousPlayerGuess playerGuesses of
+    Just guess -> return guess
+    Nothing -> liftIO $ randomRIO (0, maxNumber)
 
 getComputerFingers :: IO Int
 getComputerFingers = randomRIO (0, 5)
+
+askPlayerForGuess :: StateT GameState IO Int
+askPlayerForGuess = do
+   liftIO $ putStr "\nYour guess: "
+   guess <- liftIO (readLn :: IO Int)
+   state <- get
+   put $ state { allPlayerGuesses = guess : allPlayerGuesses state }
+   return guess
 
 askPlayerForNumber :: String -> IO Int
 askPlayerForNumber caption =
   putStr (caption ++ ": ") >> readLn :: IO Int
 
+writeln :: MonadIO m => String -> StateT a m ()
+writeln = liftIO . putStrLn
+
 game :: StateT GameState IO ()
 game = do
-  playerGuess <- liftIO $ askPlayerForNumber "\nYour guess"
+  playerGuess <- askPlayerForGuess
+
   computerGuess <- getComputerGuess 10
-  liftIO . putStrLn $ "Computer guess: " ++ show computerGuess
+  writeln $ "Computer guess: " ++ show computerGuess
+
   playerFingers <- liftIO $ askPlayerForNumber "How many fingers? "
+
   computerFingers <- getComputerGuess 5
-  liftIO . putStrLn $ "Computer showed " ++ show computerFingers ++ " fingers."
+  writeln $ "Computer showed " ++ show computerFingers ++ " fingers."
+
   let totalFingers = playerFingers + computerFingers
-  liftIO . putStrLn $ "Total fingers = " ++ show totalFingers
+  writeln $ "Total fingers = " ++ show totalFingers
+
   state <- get
   let oldScores = scores state
 
-  newScores <- liftIO $
-    if totalFingers == playerFingers
-       && totalFingers == computerFingers then do
-      putStrLn "Both guess right!"
+  newScores <-
+    if totalFingers == playerFingers && totalFingers == computerFingers then do
+      writeln "Both guess right!"
       return $ Scores (player oldScores + 1) (computer oldScores + 1)
     else if totalFingers == playerGuess then do
-      putStrLn "Your guess was right!"
+      writeln "Your guess was right!"
       return $ oldScores { player = player oldScores + 1 }
     else if totalFingers == computerGuess then do
-      putStrLn "Computer guess was right, sorry."
+      writeln "Computer guess was right, sorry."
       return $ oldScores { computer = computer oldScores + 1 }
     else do
-      putStrLn "Nobody guessed."
+      writeln "Nobody guessed."
       return oldScores
 
-  let newState = GameState newScores (playerGuess : allPlayerGuesses state)
-  liftIO . putStrLn $ "Current state: " ++ show newState
+  let newState = state { scores = newScores }
+  writeln $ "Current state: " ++ show newState
   put newState
 
-  if player newScores == 3 then
-    liftIO $ putStrLn "You win!"
-  else if computer newScores == 3 then
-    liftIO $ putStrLn "Computer wins, sorry."
-  else game
+  case (player newScores, computer newScores) of
+    (3, _) -> writeln "You win!"
+    (_, 3) -> writeln "Computer wins, sorry."
+    _ -> game
 
 main :: IO ()
 main = do
