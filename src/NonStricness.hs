3b18f3f1e3a756ba5f9c7241e7088072a109cc2d
vaskir <vaskir@gmail.com>
2016-10-05 20:29:36 +0300

    start stricness

---
+++ src/NonStricness.hs
@@ -0,0 +1,35 @@
+module NonStricness where
+
+import           Debug.Trace
+
+discriminatory :: Bool -> Int
+discriminatory b =
+  case b of
+    False -> 0
+    True -> 1
+
+x = const 1 undefined
+x' = (\_ -> 1) undefined
+y = const undefined 1
+y' = (\_ -> undefined) 1
+y'' = undefined
+z = flip const undefined 1
+z' = flip (\x _ -> x) undefined 1
+z'' = (\_ x -> x) undefined 1
+z''' = 1
+f = flip const 1 undefined
+f' = flip (\x _ -> x) 1 undefined
+f'' = (\_ x -> x) 1 undefined
+f''' = undefined
+g = const undefined undefined
+g' = (\x _ -> x) undefined undefined
+g'' = undefined
+h = foldr const 'z' ['a'..'e']
+h' = foldr (\x _ -> x) 'z' ['a'..'e']
+h'' = 'a'
+i = foldr (flip const) 'z' ['a'..'e']
+i' = foldr (\_ s -> s) 'z' ['a'..'e']
+i'' = 'z'
+
+foo = trace "calling foo" 1
+two = foo + foo
