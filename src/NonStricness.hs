f680efceaf56ea5a4e8471da7fdefbb3011f9c95
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2016-10-07 17:05:29 +0300

    non stricness

--- src/NonStricness.hs
+++ src/NonStricness.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE BangPatterns #-}
+
 module NonStricness where
 
 import           Debug.Trace
@@ -33,3 +35,18 @@ i'' = 'z'
 
 foo = trace "calling foo" 1
 two = foo + foo
+
+data List a
+  = Nil
+  | Cons !a !(List a)
+  deriving Show
+
+sTake :: Int -> List a -> List a
+sTake n _ | n <= 0 = Nil
+sTake n Nil = Nil
+sTake n (Cons x xs) = Cons x (sTake (n - 1) xs)
+
+twoEls = Cons 1 (Cons undefined Nil)
+oneEl = sTake 1 twoEls
+threeElements = Cons 2 twoEls
+oneElT = sTake 1 threeElements
